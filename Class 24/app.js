784b8088b66844a938bcab5ee5825f90ab1fe3b1
M.Shaheer Khan <160488347+Sherrrrryyy@users.noreply.github.com>
2024-03-22 16:08:06 +0500

    Add files via upload

---
+++ Class 24/app.js
@@ -0,0 +1,73 @@
+// CHAP 17-18 ARRAY AND LOOPS
+
+
+//1-
+
+// var emptyarr = [[],[],[],[]];
+// console.log(emptyarr)
+
+
+
+//2-
+
+// var muldimarr = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+// console.log(muldimarr);
+
+
+// 3-
+
+// for(i = 1; i<=10; i++){
+// console.log(i)
+// }
+
+
+// 4-
+
+// var tableNumber = +prompt("Enter a number");
+// var tableLength = +prompt("Enter a length");
+
+// for(i = 1; i<=tableLength; i++ ){
+// document.write(tableNumber + "x" + i + "=" + tableNumber * i + "<br />" )
+// }
+
+
+// 5-
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+// for(i = 0; i <= 4 ; i++){
+// document.write( "Element at index " + i + " is: " + fruits[i] + "<br />")
+// }
+
+// 6-
+
+// for(i = 1; i <= 15; i++){
+//     document.write(i + "<br />")
+// }
+
+// for(i = 15; i >= 0; i--){
+//     document.write(i + "<br />")
+
+// }
+
+// for(i = 0;  i <= 20;  i++){
+// if(i % 2 == 0){
+// document.write("even number", i + "<br />")
+// }else{
+// document.write("odd number", i + "<br />")
+// }
+// }
+
+// arr = ["pakistan", "india", "china", "canada", "australia", "newzealand", "srilinka"]
+// // arr.push = ("bangladesh");
+// arr.shift();
+
+// console.log(arr)
+
+
+
+
+
+
+
+    
+    
